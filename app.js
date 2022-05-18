63165bc5565850ce21539e50ebf38a23f686a209
ChillDiver <yumingchang1991@gmail.com>
2022-05-18 14:39:08 +0800

    improve: integrate renderIndexPageWithAlert into renderIndexPage

--- app.js
+++ app.js
@@ -6,53 +6,64 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
-// data
+const controller = {
+  returnAlertMessage(keyword) {
+    return keyword.trim().length > 0
+      ? `Oops, we couldn't find a match for '${keyword.trim()}'. Try another one?`
+      : `Seriously? Your keyword is empty... Try type something and hit search again`
+  },
+
+  isSearchQueryEmpty(req) {
+    return !req.query || req.query.keyword.trim().length === 0
+  }
+}
+
 const model = {
   async getRestaurants() {
-    let queryResult = []
-    const query = await Restaurant
+    let restaurantsFound = []
+    const restaurantsQuery = await Restaurant
       .find()
       .sort({
         rating: 'desc',
         name_en: 'asc'
        })
       .lean()
-      .then(restaurants => queryResult = restaurants.slice())
+      .then(restaurants => restaurantsFound = restaurants.slice())
       .catch(error => console.error(error))
-    return queryResult
+
+    return restaurantsFound
+  },
+
+  async getRestaurant(id) {
+    const restaurantFound = {}
+    const restaurantQuery = await Restaurant
+      .findById(id)
+      .lean()
+      .then( restaurant => Object.assign(restaurantFound, restaurant) )
+      .catch( error => console.error(error))
+
+    return restaurantFound
   },
 
   returnSearchResult(displayAlert = false, keyword = '') {
-    let finalMessage = ''
-    if (displayAlert) {
-      if (keyword.trim().length > 0) {
-        finalMessage = `Oops, we couldn't find a match for '${keyword}'. Try another one?`
-      } else {
-        finalMessage = `Seriously? Your keyword is empty... 
-        Try type something and hit search again.`
-      }
-    }
     return {
       displayAlert,
       keyword: keyword.trim(),
-      message: finalMessage
+      message: controller.returnAlertMessage(keyword.trim())
     }
-  },
+  }
 }
 
 const view = {
-  renderIndexPageWithAlert(res, keyword) {
+  renderIndexPage(res, restaurantsToRender, searchResult) {
     return res.render('index', {
-      restaurants: model.restaurants,
-      searchResult: model.returnSearchResult(true, keyword)
+      restaurants: restaurantsToRender,
+      searchResult
     })
   },
 
-  renderIndexPage(res, keyword = '', arrayToRender) {
-    return res.render('index', {
-      restaurants: arrayToRender,
-      searchResult: model.returnSearchResult(false, keyword)
-    })
+  renderShowPage(res, restaurant) {
+    return res.render('show', { restaurant })
   }
 }
 
@@ -65,43 +76,34 @@ app.use(express.static('public'))
 
 // server route
 app.get('/', async (req, res) => {
-  res.render('index', {
-    restaurants: await model.getRestaurants(),
-    searchResult: model.returnSearchResult(false)
-  })
-})
-  
-  
-
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const restaurant = model.restaurants.find(restaurant => restaurant.id.toString() === id)
-  res.render('show', { restaurant })
+  return view.renderIndexPage(res, await model.getRestaurants(), model.returnSearchResult(false))
 })
 
-app.get('/search', (req, res) => {
-  if (!req.query) {
-    return view.renderIndexPageWithAlert(res)
-  }
-  if (req.query.keyword.trim().length === 0) {
-    return view.renderIndexPageWithAlert(res)
+app.get('/restaurants/:id', async (req, res) => {
+  const restaurant = await model.getRestaurant(req.params.id)
+  return view.renderShowPage(res, restaurant)
+})
+
+app.get('/search', async (req, res) => {
+  if (controller.isSearchQueryEmpty(req)) {
+    const restaurantsToRender = []
+    const keyword = ''
+    return view.renderIndexPage(res, restaurantsToRender, model.returnSearchResult(true, keyword))
   }
   
   const keyword = req.query.keyword.trim()
   const regex = new RegExp(keyword, 'gi')
-  const restaurantsFiltered = 
-    model
-      .restaurants
+  
+  const restaurants = await model.getRestaurants()
+  const restaurantsFiltered = restaurants
       .filter(restaurant => {
           return restaurant.name.match(regex) || restaurant.name_en.match(regex) || restaurant.category.match(regex)
         })
 
-  
   if (restaurantsFiltered.length > 0) {
-    return view.renderIndexPage(res, keyword, restaurantsFiltered)
+    return view.renderIndexPage(res, restaurantsFiltered, model.returnSearchResult(false, keyword))
   }
-  return view.renderIndexPageWithAlert(res, keyword)
-
+  return view.renderIndexPage(res, restaurantsFiltered, model.returnSearchResult(true, keyword))
 })
 
 // server listen
