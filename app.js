60bfa4b44e279239d0a59718c6153d0123fbfc1d
ChillDiver <yumingchang1991@gmail.com>
2022-05-18 20:04:11 +0800

    doc: change order of returnIndexPageOptions to improve readibiltiy

--- app.js
+++ app.js
@@ -64,8 +64,8 @@ const model = {
 
   returnIndexPageOptions(displayAlert = false, keyword = '') {
     return {
-      displayAlert,
       keyword: keyword.trim(),
+      displayAlert,
       message: utils.returnAlertMessage(keyword.trim())
     }
   },
