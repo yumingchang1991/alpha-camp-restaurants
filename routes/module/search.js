dae111f0631d6ea78f7c534780cb73472d7f9155
ChillDiver <yumingchang1991@gmail.com>
2022-05-23 23:05:20 +0800

    fix: make search route and sorting together

--- routes/module/search.js
+++ routes/module/search.js
@@ -6,18 +6,11 @@ const utils = require('../../utility')
 router
   .route('/')
   .get(async (req, res) => {
-    // is query string valid ?
-    if (utils.isSearchQueryEmpty(req)) {
-      const restaurantsToRender = await model.getRestaurants(req) || []
-      const indexPageOptions = model.returnIndexPageOptions(req, true)
-      return view.renderIndexPage(res, restaurantsToRender, indexPageOptions)
-    }
-
-    // Declare variables for search
     const keyword = req.query.keyword.trim()
     const restaurantsFound = keyword.length > 0
-      ? await model.returnRestaurantsFound(req)
-      : await model.getRestaurants(req)
+      ? await model.returnRestaurantsFound(req) || []
+      : await model.getRestaurants(req) || []
+    
     const displayAlert = restaurantsFound.length === 0
     const indexPageOptions = model.returnIndexPageOptions(req, displayAlert)
     return view.renderIndexPage(res, restaurantsFound, indexPageOptions)
--- utility/index.js
+++ utility/index.js
@@ -1,13 +1,9 @@
 const utils = {
   returnAlertMessage (keyword) {
-    return keyword.trim().length > 0
-      ? `Oops, we couldn't find a match for '${keyword.trim()}'. Try another one?`
-      : 'Seriously? Your keyword is empty... Try type something and hit search again'
-  },
-
-  isSearchQueryEmpty (req) {
-    return !req.query || req.query.keyword.trim().length === 0
-  },
+    if (keyword.trim().length > 0) {
+      return `Oops, we couldn't find a match for '${keyword.trim()}'. Try another one?` 
+    }
+  }
 }
 
 module.exports = utils
