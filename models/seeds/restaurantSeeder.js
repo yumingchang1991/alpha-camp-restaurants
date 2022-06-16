f0039e96fcd6b421954b916edb1343a8a0ca5e2a
ChillDiver <yumingchang1991@gmail.com>
2022-06-16 22:35:50 +0800

    feat: change seeder to be promise-based, with friendly console messages

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -2,38 +2,74 @@ const bcrypt = require('bcryptjs')
 const mongoDb = require('../../config/mongoose')
 const Restaurant = require('../restaurant')
 const User = require('../user')
-const restaurantsJSON = require('../../restaurant.json')
+const rawJSON = require('../../restaurant.json')
 
 const SEED_USERS = [
   {
+    username: 'seed_user1',
     email: 'user1@example.com',
     password: '12345678',
-    restaurantsId: [1, 2, 3]
+    restaurantIds: [1, 2, 3]
   },
   {
+    username: 'seed_user2',
     email: 'user2@example.com',
     password: '12345678',
-    restaurantsId: [4, 5, 6]
+    restaurantIds: [4, 5, 6]
   }
 ]
 
 mongoDb.once('open', () => {
+  console.log('\n====== Seeder Program Starts ======\n')
   // Delete all documents on MongoDb
-  Restaurant
-    .find()
-    .deleteMany()
-    .then(() => {
-      console.log('All restaurants are deleted ...')
-      createSeeds()
-      process.exit()
-    })
+  initializeMongoDb()
+    .then(() => createSeedUsers())
+    .then(users => {
+      console.log('step2: creating associated restaurants for each seed user ...')
+      return Promise.all(
+        Array.from(
+          users,
+          user => {
+            const { restaurantIds } = SEED_USERS.filter(seedUser => seedUser.email === user.email)[0]
+
+            const newRestaurants = restaurantIds.map(restId => {
+              const newRestaurant = rawJSON.results.filter(restaurant => restaurant.id === restId)[0]
+              newRestaurant.userId = user._id
+              return newRestaurant
+            })
+            return Restaurant
+              .create(newRestaurants)
+          })
+      )
+    }
+  )
+    .then(() => console.log('\nDone!'))
+    .then(() => console.log('\n====== Seeder Program Ends ======\n'))
+    .then(() => mongoDb.close())
     .catch(error => console.error(error))
 })
 
-function createSeeds () {
-  Restaurant
-    .create(restaurantsJSON.results)
+function initializeMongoDb() {
+  return Promise.all(
+    [
+      Restaurant.find().deleteMany(),
+      User.find().deleteMany()
+    ]
+  )
+  .then(() => console.log('step1: deleting MongoDb data ...'))
+}
+
+function createSeedUsers() {
+  return Promise.all(Array.from(
+    SEED_USERS,
+    seedUser => {
+      // hash password then create user on MongoDB
+      return bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(seedUser.password, salt))
+        .then(hash => {
+          seedUser.password = hash
+        })
+    }))
+    .then(() => User.create(SEED_USERS))
     .catch(error => console.error(error))
-    .finally(() => mongoDb.close())
-  console.log('Seeds data are created ...')
 }
